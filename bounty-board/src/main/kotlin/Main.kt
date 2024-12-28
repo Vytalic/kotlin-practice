d0bd2b1cd1f42cedb8845ef5de3ee346c0451104
Vytalic <Vytalic>
2024-12-28 11:26:11 -0600

    Add Const variable

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
+const val HERO_NAME = "Madrigo"
+
 fun main() {
-    println("Hello World!")
+    println("The hero announces his presence to the world.")
+    println(HERO_NAME)
+    var playerLevel = 4
+    println(playerLevel)
+
+    println("The hero embarks on his journey to locate the enchanted sword.")
+    playerLevel += 1
+    println(playerLevel)
+    // page 28
+
 }
