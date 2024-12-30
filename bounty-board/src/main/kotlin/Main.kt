e800da0fdb9befd3f0380a701b37b7a27585a430
Vytalic <Vytalic>
2024-12-30 11:24:45 -0600

    Finish Chapter 5

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -1,18 +1,35 @@
 // Chapter 4
+// pg 73
 
 const val HERO_NAME = "Madrigo"
+var playerLevel = 5
 
 fun main() {
     println("The hero announces his presence to the world.")
     println(HERO_NAME)
-    var playerLevel = 4
     println(playerLevel)
 
+    readBountyBoard()
 
-    val hasBefriendedBarbarians = true
-    val hasAngeredBarbarians = false
-    val playerClass = "paladin"
-    val quest: String = when (playerLevel) {
+    println("Time passes...")
+    println("The hero returns from his quest.")
+
+    playerLevel += 1
+    println(playerLevel)
+    readBountyBoard()
+}
+
+private fun readBountyBoard() {
+    println("The hero approaches the bounty board. It reads:")
+    println(obtainQuest(playerLevel))
+}
+
+private fun obtainQuest(
+    playerLevel: Int,
+    playerClass: String = "paladin",
+    hasBefriendedBarbarians: Boolean = true,
+    hasAngeredBarbarians: Boolean = false
+): String = when (playerLevel) {
         1 -> "Meet Mr. Bubbles in the land of soft things."
         in 2..5 -> {
             // Check whether diplomacy is an option
@@ -25,20 +42,9 @@ fun main() {
                 "Save the town from the barbarian invasions."
             }
         }
+
         6 -> "Locate the enchanted sword."
         7 -> "Recover the long-lost artifact of creation."
         8 -> "Defeat Nogartse, bringer of death and eater of worlds."
         else -> "There are no quests right now."
     }
-
-    println("The hero approaches the bounty board. It reads:")
-    println(quest)
-
-
-    println("Time passes...")
-    println("The hero returns from his quest.")
-
-    playerLevel += 1
-    println(playerLevel)
-
-}
