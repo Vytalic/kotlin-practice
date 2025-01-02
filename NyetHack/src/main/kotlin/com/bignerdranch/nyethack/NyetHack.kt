2375f5b9f75bd1edfc1e1b17ddadbe1836bfdc62
Vytalic <ballenc10@gmail.com>
2025-01-02 16:53:42 -0600

    Chapter 14 - Initialization

--- NyetHack/src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -1,16 +1,21 @@
-package com.bignerdranch.nyethack// Chapter 12
+// Chapter 15
+package com.bignerdranch.nyethack
 
-
-var player = Player()
+lateinit var player: Player
 
 fun main() {
-    narrate("${player.name} is ${player.title}")
-    player.changeName("Aurelia")
+    narrate("Welcome to NyetHack!")
+    val playerName = promptHeroName()
+    player = Player(playerName)
     //changeNarratorMood()
-    narrate("${player.name}, ${player.title}, heads to the town square")
+    player.prophesize()
+    val mortality = if (player.isImmortal) "an immortal" else "a mortal"
+    narrate("${player.name} of ${player.hometown}, ${player.title}, heads to the town square")
+    narrate("${player.name}, $mortality, has ${player.healthPoints} health points")
 
     visitTavern()
     player.castFireBall()
+    player.prophesize()
 }
 
 private fun promptHeroName(): String {
--- NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Player.kt
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Player.kt
@@ -1,7 +1,12 @@
 package com.bignerdranch.nyethack
 
-class Player {
-    var name = "madrigo"
+class Player(
+    initialName: String,
+    val hometown: String = "Neversummer",
+    var healthPoints: Int,
+    val isImmortal: Boolean
+) {
+    var name = initialName
         get() = field.replaceFirstChar { it.uppercase() }
         private set(value) {
             field = value.trim()
@@ -15,6 +20,34 @@ class Player {
             else -> "The Renowned Hero"
         }
 
+    val prophecy by lazy {
+        narrate("$name embarks on an arduous quest to locate a fortune teller")
+        Thread.sleep(3000)
+        narrate("The fortune teller bestows a prophecy upon $name")
+
+        "An intrepid hero from $hometown shall some day " + listOf(
+            "form an unlikely bond between two warring factions",
+            "take possession of an otherworldy blade",
+            "bring the gift of creation back to the world",
+            "best the world-eater"
+        ).random()
+    }
+
+    init {
+        require(healthPoints > 0) { "healthpoints must be greater than zero"}
+        require(name.isNotBlank()) { "Player must have a name" }
+    }
+
+    constructor(name: String) : this(
+        initialName = name,
+        healthPoints = 100,
+        isImmortal = false
+    ) {
+        if (name.equals("Jason", ignoreCase = true)) {
+            healthPoints = 500
+        }
+    }
+
     fun castFireBall(numFireBalls: Int = 2) {
         narrate("A glass of Fireball springs into existence (x$numFireBalls)")
     }
@@ -22,5 +55,10 @@ class Player {
     fun changeName(newName: String) {
         narrate("$name legally changes their name to $newName")
         name = newName
+    }
+
+    fun prophesize() {
+        narrate("$name thinks about their future")
+        narrate("A fortune teller told $name, \"$prophecy\"")
     }
 }
